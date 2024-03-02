42a78c9a65ea127fcd5ffbfb135c152b91db741b
R045T-T045T <120365865+R045T-T045T@users.noreply.github.com>
2024-03-02 15:04:32 +0100

    Prototyp

---
+++ Tall/Assets/_TRIFORCE/Scripts/PerkInventory.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerkInventory : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void resetPerks()
+    {
+        Debug.Log("Reset Perks");
+    }
+
+    public void addPerks()
+    {
+        Debug.Log("adding Perks");
+
+    }
+
+    public void removePerks() 
+    {
+        Debug.Log("removing Perks");
+    }
+
+}
